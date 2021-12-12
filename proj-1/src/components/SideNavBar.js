f069e624c4b662efcc1e3f2bef435ebeaac09283
yogesh <yogesh.k6499@gmail.com>
2021-12-12 12:26:13 +0530

    side bar added symbols

--- proj-1/src/components/SideNavBar.js
+++ proj-1/src/components/SideNavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavItem ,Nav} from "react-bootstrap";
 import classes from './SideNavBar.module.css'
+import {AiFillHome} from 'react-icons/ai'
 
 const SideNavBar = () => {
   return <div className={classes['side-bar']}>
-
+<AiFillHome className={classes.AiFillHome} />
   </div>
 };
 
 export default SideNavBar;
+
